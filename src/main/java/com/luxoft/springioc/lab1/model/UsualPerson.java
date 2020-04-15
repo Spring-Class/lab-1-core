4043a2baf7791f8279f13d2ab56bccec7938ad2d
Dmitry Shishagin <dmitry.shishagin@mail.ru>
2020-04-15 13:15:57 +0400

    exercise1: executed exercise1 in lab1

--- src/main/java/com/luxoft/springioc/lab1/model/UsualPerson.java
+++ src/main/java/com/luxoft/springioc/lab1/model/UsualPerson.java
@@ -15,6 +15,19 @@ public class UsualPerson implements Person {
 
     private List<String> contacts;
 
+    public UsualPerson() {
+    }
+
+    public UsualPerson(Country country) {
+        this.country = country;
+    }
+
+    public UsualPerson(String name, Country country, int age) {
+        this.name = name;
+        this.country = country;
+        this.age = age;
+    }
+
     public void setIsProgrammer(boolean isProgrammer) {
         this.isProgrammer = isProgrammer;
     }
